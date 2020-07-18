9b5e5c930377a14e8ae56b43576b18eda732df99
Slahser <skyslahser@gmail.com>
2020-07-19 02:13:24 +0800

    <type [fix]>[optional scope]: rebase
    [optional body]
    
    [optional footer(s)]

--- internal/app/tt_practice/tt_practice6.go
+++ internal/app/tt_practice/tt_practice6.go
@@ -6,9 +6,51 @@ import (
 	"time"
 )
 
+func t_timer() {
+	tick := time.Tick(1e8)
+	boom := time.After(5e8)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+		default:
+			fmt.Println("    .")
+			time.Sleep(5e7)
+		}
+	}
+}
+
+func sendData(ch chan string) {
+	ch <- "Washington"
+	ch <- "Tripoli"
+	ch <- "London"
+	ch <- "Beijing"
+	ch <- "Tokyo"
+	close(ch)
+}
+
+func getData(ch chan string) {
+	//判断channel是否阻塞
+	for input := range ch {
+		fmt.Printf("%s ", input)
+	}
+
+	//for {
+	//	//判断channel是否阻塞
+	//	input, open := <-ch
+	//	if !open {
+	//		break
+	//	}
+	//	fmt.Printf("%s ", input)
+	//}
+}
+
 func suck(ch1, ch2 chan int) {
 	for {
 		select {
+		//信息按照箭头的方向流动
 		case v := <-ch1:
 			fmt.Printf(strconv.Itoa(v))
 		case v := <-ch2:
@@ -32,18 +74,26 @@ func Tt6() {
 	time.Sleep(1e9)
 	fmt.Println("At the end of main()")
 
+	//basic channel
+	//buf_ch1 := make(chan string, buf)
+	ch := make(chan string, 2)
+	go sendData(ch)
+	go getData(ch)
+	time.Sleep(1e9)
+	fmt.Println()
+
 	//有 2 个通道 ch1 和 ch2，三个协程 pump1()、pump2() 和 suck()。
 	//这是一个典型的生产者消费者模式。在无限循环中，ch1 和 ch2 通过 pump1() 和 pump2() 填充整数；
-	//suck() 也是在无限循环中轮询输入的，通过 select 语句获取 ch1 和 ch2 的整数并输出。
+	//suck() 也是在无限循环中轮询输入的，
+	//通过 select 语句获取 ch1 和 ch2 的整数并输出。
 	//选择哪一个 case 取决于哪一个通道收到了信息。程序在 main 执行 1 秒后结束。
-
 	ch1 := make(chan int)
 	ch2 := make(chan int)
-
 	go Pump1(ch1)
+	defer close(ch1)
 	go Pump2(ch2)
+	defer close(ch2)
 	go suck(ch1, ch2)
-
 	time.Sleep(1e8)
 
 }
--- internal/app/tt_practice/tt_practice7.go
+++ internal/app/tt_practice/tt_practice7.go
@@ -1,1 +1,144 @@
 package tt_practice
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+	"sync"
+)
+
+type Person2 struct {
+	Name   string
+	salary float64
+	chF    chan func()
+}
+
+func NewPerson2(name string, salary float64) *Person2 {
+	p := &Person2{name, salary, make(chan func())}
+	go p.backend()
+	return p
+}
+
+func (p *Person2) backend() {
+	for f := range p.chF {
+		f()
+	}
+}
+
+// Set salary.
+func (p *Person2) SetSalary(sal float64) {
+	p.chF <- func() { p.salary = sal }
+}
+
+// Retrieve salary.
+func (p *Person2) Salary() float64 {
+	fChan := make(chan float64)
+	p.chF <- func() { fChan <- p.salary }
+	return <-fChan
+}
+
+func (p *Person2) String() string {
+	return "Person2 - name is: " + p.Name + " - salary is: " + strconv.FormatFloat(p.Salary(), 'f', 2, 64)
+}
+
+type Task struct {
+	tt string
+	// some state
+}
+type Pool struct {
+	Mu    sync.Mutex
+	Tasks []*Task
+}
+
+func Worker1(pool *Pool) {
+	for {
+		pool.Mu.Lock()
+		// begin critical section:
+		task := pool.Tasks[0]       // take the first task
+		pool.Tasks = pool.Tasks[1:] // update the pool of tasks
+		// end critical section
+		pool.Mu.Unlock()
+		//process(task)
+		fmt.Printf(task.tt)
+	}
+}
+
+func Worker2(in, out chan *Task) {
+	for {
+		t := <-in
+		//process(t)
+		fmt.Printf(t.tt)
+		out <- t
+	}
+}
+
+func f(left, right chan int) { left <- 1 + <-right }
+
+var resume chan int
+
+/**
+只在你需要时进行求值，同时保留相关变量资源（内存和 cpu）：这是一项在需要时对表达式进行求值的技术
+通道被命名为yield和resume，这些词经常在协程代码中使用
+*/
+func integers() chan int {
+	yield := make(chan int)
+	count := 0
+	go func() {
+		for {
+			yield <- count
+			count++
+		}
+	}()
+	return yield
+}
+
+func generateInteger() int {
+	return <-resume
+}
+
+type EvalFunc func(Any) (Any, Any)
+
+/**
+- 使用锁的情景：
+    - 访问共享数据结构中的缓存信息
+    - 保存应用程序上下文和状态信息数据
+- 使用通道的情景：
+    - 与异步操作的结果进行交互
+    - 分发任务
+    - 传递数据所有权
+*/
+func Tt7() {
+	runtime.GOMAXPROCS(8)
+
+	//channel 并发访问对象
+	bs := NewPerson2("Smith Bill", 2500.5)
+	fmt.Println(bs)
+	bs.SetSalary(4000.25)
+	fmt.Println("Salary changed:")
+	fmt.Println(bs)
+
+	//pending, done := make(chan *Task), make(chan *Task)
+	//go sendWork(pending)       // put tasks with work on the channel
+	//for i := 0; i < N; i++ {   // start N goroutines to do work
+	//	go Worker(pending, done)
+	//}
+	//consumeWork(done)          // continue with the processed tasks
+
+	//链式goroutine
+	leftmost := make(chan int)
+	var left, right chan int = nil, leftmost
+	for i := 0; i < 100; i++ {
+		left, right = right, make(chan int)
+		go f(left, right)
+	}
+	right <- 0      // bang!
+	x := <-leftmost // wait for completion
+	fmt.Println(x)  // 100000, ongeveer 1,5 s
+
+	//lazy generator
+	resume = integers()
+	fmt.Println(generateInteger()) //=> 0
+	fmt.Println(generateInteger()) //=> 1
+	fmt.Println(generateInteger()) //=> 2
+
+}
