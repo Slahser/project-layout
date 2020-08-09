58f6f26d362c2e8ef2fb6c97699c70c8b8af9bbd
Slahser <skyslahser@gmail.com>
2020-08-09 23:21:37 +0800

    <type BREAKING CHANGE>cmd: add some cmd package
    [optional body]
    
    [optional footer(s)]

---
+++ cmd/oe/main/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	prompt "github.com/c-bata/go-prompt"
+	gosxnotifier "github.com/deckarep/gosx-notifier"
+	banner "github.com/dimiro1/banner"
+	asciigraph "github.com/guptarohit/asciigraph"
+	table "github.com/jedib0t/go-pretty/table"
+	opts "github.com/jpillora/opts"
+	cli "github.com/urfave/cli/v2"
+)
+
+//go-prompt
+func completer(d prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "users", Description: "Store the username and age"},
+		{Text: "articles", Description: "Store the article text posted by user"},
+		{Text: "comments", Description: "Store the text commented to articles"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
+func main() {
+	//banner start
+	nyanFilePath := "assets/nyancat.txt"
+	nyanBuf, err := ioutil.ReadFile(nyanFilePath)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "File Error: %s\n", err)
+		// panic(err.Error())
+	}
+
+	banner.Init(os.Stdout, true, true, bytes.NewBuffer(nyanBuf))
+	//banner stop
+
+	//go-prompt start
+	fmt.Println("go-prompt demo,Please select item.")
+	t := prompt.Input("> ", completer)
+	fmt.Println("You selected " + t)
+	//go-prompt end
+
+	//At a minimum specifiy a message to display to end-user.
+	note := gosxnotifier.NewNotification("tt") //Optionally, set a title
+	note.Title = "tt override!!! 💰"
+
+	//Optionally, set a subtitle
+	note.Subtitle = "tt subtitle"
+	//Optionally, set a sound from a predefined set.
+	note.Sound = gosxnotifier.Basso
+
+	//Optionally, specifiy a url or bundleid to open should the notification be
+	//clicked.
+	note.Link = "https://www.google.com" //or BundleID like: com.apple.Terminal
+
+	//Optionally, an app icon (10.9+ ONLY)
+	note.AppIcon = "gopher.png"
+
+	//Optionally, a content image (10.9+ ONLY)
+	note.ContentImage = "gopher.png"
+
+	//If necessary, check error
+	if err := note.Push(); err != nil {
+		log.Println("Uh oh!")
+	}
+
+	typicalMap := make(map[string]string)
+	typicalMap["-a"] = "All, append"
+	typicalMap["-b"] = "Buffer,block size, batch"
+	typicalMap["-c"] = "Command, check"
+	typicalMap["-d"] = "Debug, delete, directory"
+	typicalMap["-D"] = "Define"
+	typicalMap["-e"] = "Execute, edit"
+	typicalMap["-f"] = "File, force"
+	typicalMap["-h"] = "Headers, help"
+	typicalMap["-i"] = "Initialize"
+	typicalMap["-I"] = "Include"
+	typicalMap["-k"] = "Keep, kill"
+	typicalMap["-l"] = "List, long, load"
+	typicalMap["-m"] = "Message"
+	typicalMap["-n"] = "Number, not"
+	typicalMap["-o"] = "Output"
+	typicalMap["-p"] = "Port, protocol"
+	typicalMap["-q"] = "Quiet"
+	typicalMap["-r"] = "Recurse, reverse"
+	typicalMap["-s"] = "Silent, subject"
+	typicalMap["-t"] = "Tag"
+	typicalMap["-u"] = "User"
+	typicalMap["-v"] = "Verbose"
+	typicalMap["-V"] = "Version"
+	typicalMap["-w"] = "Width, warning"
+	typicalMap["-x"] = "Enable debugging, extract"
+	typicalMap["-y"] = "Yes"
+	typicalMap["-z"] = "Enable compression"
+
+	//table start
+	rt := table.NewWriter()
+	rt.SetOutputMirror(os.Stdout)
+	rt.AppendHeader(table.Row{"Option", "Typical meaning"})
+	for op, tm := range typicalMap {
+		rt.AppendRow([]interface{}{op, tm})
+	}
+	rt.AppendRow([]interface{}{})
+	rt.Render()
+	//table stop
+
+	//opts start
+	//https://github.com/jpillora/opts 相比之下这个更好一些
+	//https://github.com/jessevdk/go-flags
+	c := config{}
+	opts.Parse(&c)
+	log.Printf("%+v", c)
+	//opts stop
+
+	//cli start most famous
+	//https://github.com/urfave/cli/blob/master/docs/v2/manual.md
+	app := &cli.App{
+		Name:  "boom",
+		Usage: "make an explosive entrance",
+		Action: func(c *cli.Context) error {
+			fmt.Println("boom! I say!")
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+	//cli stop
+
+	//asciigraph start
+	data := []float64{3, 4, 9, 6, 2, 4, 5, 8, 5, 10, 2, 7, 2, 5, 6}
+	graph := asciigraph.Plot(data)
+
+	fmt.Println(graph)
+	//asciigraph stop
+}
+
+/**
+$ go build -o my-prog
+$ ./my-prog --help
+
+  Usage: my-prog [options]
+
+  Options:
+  --file, -f   file to load
+  --lines, -l  number of lines to show
+  --help, -h   display help
+
+$ ./my-prog -f foo.txt -l 42
+{File:foo.txt Lines:42}
+*/
+type config struct {
+	File  string `opts:"help=file to load"`
+	Lines int    `opts:"help=number of lines to show"`
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,21 @@ module github.com/Slahser/coup-de-grace
 go 1.14
 
 require (
+	github.com/c-bata/go-prompt v0.2.3
 	github.com/cch123/gomod-conflict-detect v0.0.0-20200429095045-c20632c5c90f // indirect
+	github.com/deckarep/gosx-notifier v0.0.0-20180201035817-e127226297fb
 	github.com/dimiro1/banner v1.0.0
 	github.com/felixge/fgprof v0.9.0
+	github.com/go-openapi/strfmt v0.19.5 // indirect
 	github.com/goproxy/goproxy v0.1.5
+	github.com/guptarohit/asciigraph v0.5.0
+	github.com/jedib0t/go-pretty v4.3.0+incompatible
+	github.com/jpillora/opts v1.2.0
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mattn/go-tty v0.0.3 // indirect
+	github.com/pkg/term v0.0.0-20200520122047-c3ffed290a03 // indirect
 	github.com/sirupsen/logrus v1.6.0
+	github.com/urfave/cli/v2 v2.2.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
