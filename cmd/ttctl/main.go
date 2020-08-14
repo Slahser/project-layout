d7580f3cc64a55a1857d725c3735ad85dc65a65b
S1ahs3r <skyslahser@gmail.com>
2020-08-14 17:48:48 +0800

    tt

--- cmd/ttctl/main.go
+++ cmd/ttctl/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 
 	jsoniter "github.com/json-iterator/go"
-
-	"github.com/mitchellh/go-homedir"
 
 	"github.com/Slahser/coup-de-grace/cmd/ttctl/app"
 )
@@ -18,36 +14,8 @@ var (
 
 func main() {
 
-	show()
-
 	if err := app.Run(); err != nil {
 		os.Exit(1)
 	}
 	os.Exit(0)
-}
-
-func show() {
-
-	basicPath, _ := homedir.Dir()
-	inputFile, inputError := os.Open(basicPath + "/.ttctl/workspace.json")
-	if inputError != nil {
-		fmt.Printf("An error occurred on opening the inputfile")
-		return
-	}
-	defer inputFile.Close()
-
-	inputBytes, _ := ioutil.ReadAll(inputFile)
-
-	var wsConfig workspaceConfig
-
-	_ = json.Unmarshal(inputBytes, &wsConfig)
-
-	serializedConfig, _ := json.MarshalIndent(wsConfig, "", "    ")
-	fmt.Printf(string(serializedConfig))
-}
-
-type workspaceConfig struct {
-	UserId       string `json:"user_id"`
-	Organization string `json:"organization"`
-	Project      string `json:"project"`
 }
